3b3bd9046ec0f5e1a8b4e9937fb88b99a01a805e
Ross <ross.amory98@gmail.com>
2026-03-19 23:15:40 +0000

    Added docstrings to the HPC file

--- pyMD/tools/hpc.py
+++ pyMD/tools/hpc.py
@@ -1,6 +1,18 @@
 import os 
 
 class partitionClass:
+    """
+    Defines the default data for a HPC SLURM partition.
+        
+    Attributes:
+        name (str): Name of the partition. 
+        cpus_per_node (int): Maximum number of CPUs on a node.
+        gpus_per_node (int): Maximum number of GPUs on a node.
+        mem_per_node (int): Maximum ammount of RAM per node in GB.
+        nodes (int): Maximum number of nodes in partition.
+        walltime (int): Maximum ammount of time allowed in the partition in hours.
+        qos (str|None): Quality of Service if required. Defaults to None.
+    """
     cpus_per_node: int
     mem_per_node: int
     gpus_per_node: int
@@ -16,6 +28,17 @@ class partitionClass:
                  nodes: int, 
                  walltime: int,
                  qos: str|None = None) -> None:
+        """Defines the default data for a HPC SLURM partition.
+        
+        Args:
+            name (str): Name of the partition. 
+            cpus_per_node (int): Maximum number of CPUs on a node.
+            gpus_per_node (int): Maximum number of GPUs on a node.
+            mem_per_node (int): Maximum ammount of RAM per node in GB.
+            nodes (int): Maximum number of nodes in partition.
+            walltime (int): Maximum ammount of time allowed in the partition in hours.
+            qos (str|None): Quality of Service if required. Defaults to None.
+        """
         self.name = name
         self.cpus_per_node = cpus_per_node
         self.gpus_per_node = gpus_per_node
@@ -24,17 +47,47 @@ class partitionClass:
         self.walltime = walltime
         self.qos = qos
 
+    def __str__(self) -> str:
+        return f"Partition: {self.name}"
+
 
 class HPC:
-    partitions: dict = dict()
+    """Defines a HPC cluster. This class contains all important information to how it is setup. 
+
+    Attributes:
+        partitions (dict[str,partitionClass]): Dictionary of all partitions
+        login_node (str): Hostname of the HPC
+        name (str): Name of the HPC
+        username (str): Username to connect to the HPC
+    """
+    partitions: dict[str,partitionClass] = dict()
     login_node: str
     name: str
-    
-    def __init__(self, name: str, login_node: str) -> None:
+    username: str
+
+    def __init__(self, name: str, login_node: str, username: str = os.getlogin()) -> None:
+        """Initialises the HPC class. This allows allocation of name and how to connect
+
+        Args:
+            name (str): Name of the HPC
+            login_node (str): Hostname of the HPC. (How to connect)
+            username (str, optional): username to connect to the HPC. Defaults to the current username.
+        """
         self.name = name
         self.login_node = login_node
+        self.username = username
 
-    def add_partition(self, partition: partitionClass) -> None: 
+
+    def __str__(self) -> str:
+        return f"HPC: {self.name}"
+
+
+    def add_partition(self, partition: partitionClass) -> None:
+        """Adds partitions to the HPC.
+
+        Args:
+            partition (partitionClass): Partition to add
+        """
         self.partitions[partition.name] = partition
 
 
@@ -48,54 +101,113 @@ ADA.add_partition(partitionClass("compchemq", 56, 4, 384, 5, 168, "compchem"))
 
 
 class slurm:
+    """Contains information for a slurm job. This allows for checking that the configuration is allowed. 
+
+    Attributes:
+       hpc (HPC): HPC to use
+       partition (partitionClass): Partition to use
+       time (int): Job Wall time in hours
+       name (str): Name of the job
+       nodes (int): Number of nodes to use
+       mem (int): Memory per node in GB
+       gpus (int): Number of gpus to use
+       modules (list[str]): Modules to load using the "modules system"
+       array (bool): Whether to use an array job
+       array_len (int):
+       array_file (str): File to use for the array job
+       tasks_pn (int): slurm NTASKS_PER_NODE variable
+    """
     hpc: HPC = ADA
     partition: partitionClass  = ADA.partitions["defq"]
     time = 24
-    name = "Submission"
+    name = "pyMD"
     nodes = 1
     mem = 10
     gpus = 0
     modules: list[str]
     array: bool = False
     array_len: int
-    array_file: str
+    array_file: str|None = None
     tasks_pn: int
 
     def __init__(self,partition:str = "defq"):
+        """Initialises the job, by first selecting the partition
+        
+        Args:
+            partition (str, optional): Partition to use. Defaults to defq.
+        """
         assert partition in self.hpc.partitions.keys(), f"Partition {partition} not recognised for HPC {self.hpc.name}."
         self.partition = HPC.partitions[partition]
 
 
+    def __str__(self) -> str:
+        return f"{self.name} SLURM JOB on {self.hpc.name} on {self.partition}"
+
     def set_time(self, time:int):
+        """Sets the maximum ammount of time for the slurm job. 
+
+        Args:
+           time (int): Maximum time for the slurm job.
+        """
         assert time <= self.partition.walltime, f"Time {time} exceeds max walltime for partition {self.partition} on HPC {self.hpc}."
         self.time = time
 
 
     def set_name(self, name:str):
+        """Defines the job name
+
+        Args:
+            name (str): name of the job.
+        """
         self.name = name
 
 
     def set_nodes(self, nodes:int):
+        """Sets the number of nodes to use in the job.
+
+        Args:
+            nodes (int): Number of nodes to use
+        """
         assert nodes <= self.partition.nodes, f"Nodes {nodes} exceeds max nodes for partition {self.partition} on HPC {self.hpc}."
         self.nodes = nodes
 
 
     def set_mem(self, mem:int):
+        """Allocates the memory for the job.
+
+        Args:
+           mem (int): Memory to allocate for the job.
+        """
         assert mem <= self.partition.mem_per_node, f"Memory {mem} exceeds max memory for partition {self.partition} on HPC {self.hpc}."
         self.mem = mem
 
 
     def set_gpus(self, gpus:int):
+        """Allocates the number of GPUs for the job
+
+       Args:
+            gpus (int): Number of GPU's for the job.
+        """
         assert gpus <= self.partition.gpus_per_node, f"GPUs {gpus} exceeds max GPUs for partition {self.partition} on HPC {self.hpc}."
         self.gpus = gpus
 
 
     def set_modules(self, modules:list[str]):
+        """Allocates the modules that are required for the job
+
+        Args:
+            modules (list[str]): List of modules to be loaded for the job. #TODO Add a way to define modules that are available on the HPC.
+        """
         self.modules = modules
 
 
     def set_ntasks(self, tasks:int, per_node:bool = True):
-    
+        """Allocates the SLURM_NTASKS for the job
+
+        Args:
+            tasks (int): Number of tasks for the job.
+            per_node (bool, optional): Whether to set the SLURM_NTASKS_PER_NODE for the job. Defaults to True. #TODO implement False
+        """
         if per_node:
             self.tasks_pn = tasks
         else:
@@ -104,6 +216,13 @@ class slurm:
 
 
     def set_array(self, array_file:str, length: int|None = None, check:bool = True):
+        """Enables the use of array jobs
+        
+        Args:
+            array_file (str): Path to file where each line is a job.
+            length (int|None): The length of the array job. If None, the length is determined from the array file. 
+            check (bool): Whether to check if the array file exists. Defaults to True.
+            """
         if check:
             assert os.path.isfile(array_file), f"Array file {array_file} not found."
         self.array = True
@@ -116,7 +235,15 @@ class slurm:
         self.array_file = array_file
 
 
-    def gen_script(self, command:str = "")->str:
+    def gen_script(self, command:str)->str:
+        """Generates the SLURM script for the job.
+
+        Args:
+            command (str): The command to run. 
+        
+        Returns:
+            str: The SLURM file
+        """
         file = f"""#!/bin/bash
 #SBATCH --job-name={self.name}
 #SBATCH --partition={self.partition.name}
